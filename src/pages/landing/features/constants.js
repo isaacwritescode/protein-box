ca48aaa06454fd379573db07a3b7d0792fac596f
Isaac Fernandes <isaacwritescode@protonmail.com>
2023-08-31 15:39:09 +0530

    make features section responsive

--- src/pages/landing/features/constants.js
+++ src/pages/landing/features/constants.js
@@ -1,23 +1,25 @@
 const FEATURES = [
   {
-    label: "Thumb rule",
-    title: "Weight loss and body building is 70% diet and 30% exercise",
+    label: "Widely available",
+    title: "Find us on Swiggy and Zomato or order directly from us",
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
-    img: "/images/features/03.png"
+    img: "/images/features/03.png",
+    mobileImg: "/images/features/03-mobile.png"
   },
   {
     label: "Wide variety",
     title: "Our menu is designed to meet all your protein goals, tastefully",
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
-    img: "/images/features/02.png"
+    img: "/images/features/02.png",
+    mobileImg: "/images/features/02-mobile.png"
   },
   {
     label: "Widely available",
     title: "Find us on Swiggy and Zomato or order directly from us",
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
-    img: "/images/features/03.png"
+    img: "/images/features/03.png",
+    mobileImg: "/images/features/03-mobile.png"
   },
-  
 ]
 
 export default FEATURES;
--- src/pages/landing/features/index.js
+++ src/pages/landing/features/index.js
@@ -1,20 +1,25 @@
-import { Typography } from "@mui/material";
+import { useTheme } from "@emotion/react";
+import { Typography, useMediaQuery } from "@mui/material";
 import { Box, Stack } from "@mui/system";
 import FEATURES from "./constants";
 
 export default function Features() {
-  return <Box maxWidth={1000} mx="auto" >
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down('md'));
+  console.log(matches)
+
+  return <Box maxWidth={1000} mx="auto" width={{ xs: "90%", md: "100%" }}>
     <Stack spacing={12} py={12}>
-      {FEATURES.map(({ label, title, desc, img }, idx) =>
+      {FEATURES.map(({ label, title, desc, img, mobileImg }, idx) =>
         <Box key={idx}>
-          <Stack direction={idx % 2 === 1 ? "row-reverse" : "row"} spacing={{ xs: 3, md: 6 }} alignItems="center">
+          <Stack direction={matches ? "column" : idx % 2 === 1 ? "row-reverse" : "row"} spacing={{ xs: 3, md: 6 }} alignItems="center">
             <Stack alignItems="start" gap={1} flex={2}>
               <Typography variant="body1" textTransform="uppercase" fontWeight={500} color="accent.main">{label}</Typography>
               <Typography variant="h3">{title}</Typography>
               <Typography variant="body1" fontWeight={500} color="secondary">{desc}</Typography>
             </Stack>
             <Stack flex={2}>
-              <img src={img} alt={label} width="100%" />
+              <img src={matches ? mobileImg : img} alt={label} width="100%" />
             </Stack>
           </Stack>
         </Box>
