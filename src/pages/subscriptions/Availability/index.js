9b13c6f70f42fabe52f57d51da4af1266675e62f
Isaac Fernandes <isaacwritescode@protonmail.com>
2023-09-02 01:36:25 +0530

    availability section

---
+++ src/pages/subscriptions/Availability/index.js
@@ -0,0 +1,15 @@
+import { Box } from "@mui/material";
+import Header from "../../../components/Header";
+
+export default function Availability() {
+  return <Box maxWidth={1000} m="auto" width={{ xs: "90%", md: "100%" }} pt={16}>
+    <Header
+      label="Widely available"
+      title="Find us on Swiggy & Zomato"
+    />
+
+    <Box mt={8}>
+      <img src="/images/availability/Z&S.png" width="100%" alt="" />
+    </Box>
+  </Box>
+}
--- src/pages/subscriptions/index.js
+++ src/pages/subscriptions/index.js
@@ -1,9 +1,11 @@
 import Footer from "../../components/Footer";
+import Availability from "./Availability";
 import Main from "./Main";
 
 export default function Subscriptions() {
   return <>
     <Main />
+    <Availability />
     <Footer />
   </>
 }
