b09408687863a9bb285bc6428f9027ae4d9f7bd0
Isaac Fernandes <isaacwritescode@protonmail.com>
2023-09-02 15:56:47 +0530

    added cards to feature section

--- src/pages/landing/features/constants.js
+++ src/pages/landing/features/constants.js
@@ -1,3 +1,5 @@
+import { Health } from "iconsax-react";
+
 const FEATURES = [
   {
     label: "Widely available",
@@ -11,7 +13,24 @@ const FEATURES = [
     title: "Our menu is designed to meet all your protein goals, tastefully",
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
     img: "/images/features/02.png",
-    mobileImg: "/images/features/02-mobile.png"
+    mobileImg: "/images/features/02-mobile.png",
+    offerings: [
+      {
+        icon: <Health />,
+        title: "Clean & Safe",
+        desc: "Lorem ipsum dolor sit amet, consectetur"
+      },
+      {
+        icon: <Health />,
+        title: "Structured diets",
+        desc: "Lorem ipsum dolor sit amet, consectetur"
+      },
+      {
+        icon: <Health />,
+        title: "Monthly Subscriptions",
+        desc: "Lorem ipsum dolor sit amet, consectetur"
+      },
+    ]
   },
   {
     label: "Widely available",
--- src/pages/landing/features/index.js
+++ src/pages/landing/features/index.js
@@ -1,6 +1,7 @@
 import { useTheme } from "@emotion/react";
-import { Typography, useMediaQuery } from "@mui/material";
+import { Grid, Typography, useMediaQuery } from "@mui/material";
 import { Box, Stack } from "@mui/system";
+import { cloneElement } from "react";
 import FEATURES from "./constants";
 
 export default function Features() {
@@ -8,9 +9,9 @@ export default function Features() {
   const matches = useMediaQuery(theme.breakpoints.down('md'));
   console.log(matches)
 
-  return <Box maxWidth={1000} mx="auto" width={{ xs: "90%", md: "100%" }}>
+  return <Box maxWidth={1000} mx="auto" width={{ xs: "90%", lg: "100%" }}>
     <Stack spacing={12} py={12}>
-      {FEATURES.map(({ label, title, desc, img, mobileImg }, idx) =>
+      {FEATURES.map(({ label, title, desc, img, mobileImg, offerings }, idx) =>
         <Box key={idx}>
           <Stack direction={matches ? "column" : idx % 2 === 1 ? "row-reverse" : "row"} spacing={{ xs: 3, md: 6 }} alignItems="center">
             <Stack alignItems="start" gap={1} flex={2}>
@@ -22,6 +23,25 @@ export default function Features() {
               <img src={matches ? mobileImg : img} alt={label} width="100%" />
             </Stack>
           </Stack>
+          {offerings && <Grid container columns={{ xs: 4, sm: 8, md: 12 }} mt={8} spacing={2} alignItems="center">
+            {offerings.map(({ icon, title, desc }, idx) => {
+              const accent = theme.palette.accent.main;
+              icon = cloneElement(icon, {
+                color: accent
+              })
+              return <Grid item xs={4} key={idx}>
+                <Stack position="relative" spacing={3} direction="row" bgcolor="#ffffff05" borderRadius={2} p={3} border="1px solid #ffffff10" alignItems="center">
+                  <Stack sx={{ aspectRatio: "1/1", width: 64 }} borderRadius={2} border={`1px solid ${accent}`} alignItems="center" justifyContent="center" bgcolor={accent + "10"}>
+                    {icon}
+                  </Stack>
+                  <Stack>
+                    <Typography variant="body1">{title}</Typography>
+                    <Typography variant="body2" color="secondary">{desc}</Typography>
+                  </Stack>
+                </Stack>
+              </Grid>
+            })}
+          </Grid>}
         </Box>
       )
       }
