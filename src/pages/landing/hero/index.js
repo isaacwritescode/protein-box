e3458bd5dbde847333e57bf290a70c5c8e6d7562
Isaac Fernandes <isaacwritescode@protonmail.com>
2023-10-28 11:42:37 +0530

    add bg video

--- src/pages/landing/hero/index.js
+++ src/pages/landing/hero/index.js
@@ -2,31 +2,47 @@ import { Box, Button, Stack, Typography } from "@mui/material";
 import { Link } from "../../../components/Link";
 
 export default function Hero() {
-  return <Stack
-    justifyContent="center"
-    style={{
-      backgroundImage: "url('/images/hero-bg.png')",
-      backgroundSize: "cover",
-      backgroundPosition: "top",
-      backgroundRepeat: "no-repeat",
-      height: "100vh",
-    }}
-  >
-    <Stack data-sal="fade-up"
-      textAlign="center" justifyContent="center" alignItems="center" gap={2}>
-      <Typography textTransform="uppercase">We are</Typography>
-      <Box>
-        <img src="/images/logo.png" width="100%" />
-      </Box>
-      <Typography>
-        Protein Perfection Delivered to Your Doorstep.
-      </Typography>
-      <Link
-        path="/subscriptions"
-        component={
-          <Button variant="contained">Get yours now</Button>
-        }
-      />
+  return (
+    <Stack
+      position="relative"
+      height="100vh"
+      width="100%"
+      justifyContent="center"
+      overflow="hidden"
+    >
+      <video
+        id="videoBG"
+        style={{
+          opacity: 0.5,
+          position: "absolute",
+          top: 0,
+          left: "50%",
+          transform: "translateX(-50%)",
+          zIndex: -1
+        }}
+        autoPlay={true}
+        loop
+        muted
+      >
+        <source src="/videos/bg.mp4" type="video/mp4" />
+      </video>
+      <Stack
+        data-sal="fade-up"
+        textAlign="center"
+        justifyContent="center"
+        alignItems="center"
+        gap={2}
+      >
+        <Typography textTransform="uppercase">We are</Typography>
+        <Box>
+          <img src="/images/logo.png" width="100%" />
+        </Box>
+        <Typography>Protein Perfection Delivered to Your Doorstep.</Typography>
+        <Link
+          path="/subscriptions"
+          component={<Button variant="contained">Get yours now</Button>}
+        />
+      </Stack>
     </Stack>
-  </Stack>
+  );
 }
--- src/pages/landing/sponsors/index.js
+++ src/pages/landing/sponsors/index.js
@@ -3,25 +3,32 @@ import SPONSORS from "./constants";
 
 export default function Sponsors() {
   return (
-    <Stack
-      width={{ xs: "90%", lg: "100%" }}
-      direction="row"
-      alignItems="center"
-      justifyContent="space-between"
-      maxWidth={1000}
-      mx="auto"
-      gap={{ xs: 4, md: 8 }}
-      py={8}
-      boxSizing="border-box"
-      flexWrap="wrap"
-    >
-      {SPONSORS.map(({ title, desc, icon }, idx) => (
-        <Box key={idx} data-sal="fade-up" data-sal-delay={idx * 100} sx={{cursor: "pointer"}}>
-          <Tooltip title={desc}>
-            <img src={icon} height={48} alt={title} width="100%" />
-          </Tooltip>
-        </Box>
-      ))}
+    <Stack width="100%" bgcolor="#1a1c1e">
+      <Stack
+        width={{ xs: "90%", lg: "100%" }}
+        direction="row"
+        alignItems="center"
+        justifyContent="space-between"
+        maxWidth={1000}
+        mx="auto"
+        gap={{ xs: 4, md: 8 }}
+        py={8}
+        boxSizing="border-box"
+        flexWrap="wrap"
+      >
+        {SPONSORS.map(({ title, desc, icon }, idx) => (
+          <Box
+            key={idx}
+            data-sal="fade-up"
+            data-sal-delay={idx * 100}
+            sx={{ cursor: "pointer" }}
+          >
+            <Tooltip title={desc}>
+              <img src={icon} height={48} alt={title} width="100%" />
+            </Tooltip>
+          </Box>
+        ))}
+      </Stack>
     </Stack>
   );
 }
