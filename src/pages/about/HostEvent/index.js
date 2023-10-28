3a37f7f58ff82b9e50bc3481401d05a07cf86d85
Isaac Fernandes <isaacwritescode@protonmail.com>
2023-10-28 14:03:08 +0530

    enable forms

--- src/pages/about/HostEvent/index.js
+++ src/pages/about/HostEvent/index.js
@@ -1,75 +1,66 @@
-import { Box, Button, Grid, Stack, TextField, Typography, useMediaQuery, useTheme } from "@mui/material";
+import {
+  Box,
+  Button,
+  Grid,
+  Stack,
+  TextField,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 import Header from "../../../components/Header";
 
 export default function HostEvent() {
   const theme = useTheme();
   const sm = useMediaQuery(theme.breakpoints.up("sm"));
 
-  return <Box maxWidth={1000} m="auto" width={{ xs: "90%", md: "100%" }} pt={12}>
-    <Header
-      label="Host an event"
-      title="Start a Protein Box Revolution!"
-    />
-    <Box mt={8} data-sal="fade-up">
-      <Grid container columns={{ xs: 6, md: 12 }} spacing={8} alignItems="center">
-        <Grid item xs={6}>
-          <Typography variant={sm ? "h4" : "h5"} >
-            Register now
-          </Typography>
-          <Typography variant="body1" fontWeight={500} width="80%" color="secondary">
-           Fill out your details & we'll get back to you shortly. We're glad you're here!
-          </Typography>
-        </Grid>
-        <Grid item xs={6}>
-          <form name="contact" method="POST" data-netlify="true">
-            <input type="hidden" name="form-name" value="contact" />
-            <Stack
-              spacing={4}
-              borderRadius={2}
-              width="100%"
-            >
-              <Stack direction="row" spacing={2}>
-                <TextField
-                  sx={{ width: "100%" }}
-                  label="First Name"
-                  name="first-name"
-                  variant="outlined"
-                  required
-                />
-                <TextField
-                  sx={{ width: "100%" }}
-                  label="Last Name"
-                  name="last-name"
-                  variant="outlined"
-                  required
-                />
-              </Stack>
+  return (
+    <Box maxWidth={1000} m="auto" width={{ xs: "90%", md: "100%" }} pt={12}>
+      <Header label="Host an event" title="Start a Protein Box Revolution!" />
+      <Box my={8} data-sal="fade-up" maxWidth={800} mx="auto">
+        <form name="host-event" method="POST" data-netlify="true">
+          <input type="hidden" name="form-name" value="contact" />
+          <Stack spacing={4} borderRadius={2} width="100%">
+            <Stack direction="row" spacing={2}>
               <TextField
-                label="Email"
-                name="email"
-                type="email"
+                sx={{ width: "100%" }}
+                label="First Name"
+                name="first-name"
                 variant="outlined"
                 required
               />
               <TextField
-                label="Message (Optional)"
-                name="message"
-                rows={4}
+                sx={{ width: "100%" }}
+                label="Last Name"
+                name="last-name"
                 variant="outlined"
-                multiline
+                required
               />
-              <Button
-                type="submit"
-                variant="contained"
-                size="large"
-                sx={{ width: "fit-content" }}
-              >
-                Send message
-              </Button>
             </Stack>
-          </form>
-        </Grid>
-      </Grid>
+            <TextField
+              label="Email"
+              name="email"
+              type="email"
+              variant="outlined"
+              required
+            />
+            <TextField
+              label="Message (Optional)"
+              name="message"
+              rows={4}
+              variant="outlined"
+              multiline
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              size="large"
+            >
+              Send message
+            </Button>
+          </Stack>
+        </form>
+      </Box>
     </Box>
-  </Box>
+  );
 }
