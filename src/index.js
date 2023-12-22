3dcd100ef19e6715c76523d2beb33ac090a47e92
Isaac Fernandes <isaacwritescode@protonmail.com>
2023-12-22 12:51:38 +0530

    undo

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ScrollToTop />
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>
