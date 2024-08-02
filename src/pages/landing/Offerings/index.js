2368426279ed7ddaaf187d1da5d751683296ed36
Isaac Fernandes <isaacwritescode@protonmail.com>
2024-08-02 22:46:07 +0530

    add sticker

--- src/pages/landing/Offerings/index.js
+++ src/pages/landing/Offerings/index.js
@@ -1,26 +1,18 @@
-import { Box, Grid, Stack, Typography, useTheme } from "@mui/material";
-import { cloneElement } from "react";
+import { Box, Stack, Typography } from "@mui/material";
 import Header from "../../../components/Header";
 import FEATURES from "./constants";
-import {
-  Check,
-  CheckCircle,
-  CheckCircleOutlineRounded,
-} from "@mui/icons-material";
 import Marquee from "react-fast-marquee";
 
 export default function Offerings() {
-  const theme = useTheme();
   return (
-    <Box maxWidth={1000} m="auto" width={{ xs: "90%", lg: "100%" }} py={12}>
+    <Box width="100%" py={12}>
       <Header
         label="Benefits"
         title="Nourish Your Body, Elevate Your Life: Protein Box Benefits"
       />
-      <Box mt={8}>
+      <Box mt={12}>
         <Marquee>
           {FEATURES.map(({ title, desc, gradient }, idx) => {
-            const accent = theme.palette.accent.main;
             return (
               <Box
                 ml={4}
